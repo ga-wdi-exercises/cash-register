6687e8c47f616daecba6eebb209336273bb6679d
Andy <andrew.sunglae.kim@gmail.com>
2016-04-01 15:56:54 -0400

    finish solution

--- js/main.js
+++ js/main.js
@@ -1,31 +1,46 @@
 $(document).ready(function(){
   var cashRegister = {
-    entries: [],
+    total: 0,
     elements: {
       form: $("#entry"),
+      input: $("#newEntry"),
       entries: $("#entries"),
-      total: $("#newEntry")
+      totalTD: $("#total")
     },
-    calculateTotal: function(){
-      var total = 0;
-      for( var i = 0; i < this.entries.length; i++ ){
-        total += this.entries[i];
-      }
-      return total;
+    addToTotal: function(amount){
+      this.total += amount
     },
     addEntry: function(){
+      this.addToTotal(parseFloat(this.getEntryAmount()))
+      this.elements.entries.append(this.renderEntry())
+      this.renderTotal()
+    },
+    getEntryAmount: function(){
+      var input = this.getInput()
+      var entryAmount = parseFloat(input).toFixed(2)
+      return entryAmount
+    },
+    renderEntry: function(){
+      var entryAmount = this.getEntryAmount()
+      this.elements.input.val("")
+      console.log(entryAmount)
       var newEntry = $("<tr></tr>")
+      var entry = $("<td></td>")
+      entry.html(entryAmount)
       newEntry.append($("<td></td>"))
-
+      newEntry.append(entry)
+      return newEntry
+    },
+    renderTotal: function(){
+      this.elements.totalTD.html("$" + this.total.toFixed(2))
     },
     getInput: function(){
-      var input = this.elements.form.value()
-      return input
+      return this.elements.input.val()
     }
   }
 
   cashRegister.elements.form.on("submit", function(e){
     e.preventDefault()
-    console.log("submit works")
+    cashRegister.addEntry()
   })
 })
