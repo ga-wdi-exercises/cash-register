05a070a3d34008ddef4f45df6cc0e37d0cd30321
Megan Coyle <meg@mcoyle.com>
2016-07-06 18:30:51 -0400

    Attempt to get removing and updating entries to work

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
   var $form = $("#entry");
   var $entries = $("#entries");
   var total = 0;
+  var $tbody = $('tbody');
 
   $form.on("submit", cashRegisterIt);
 
@@ -11,12 +12,31 @@ $( document ).ready(function() {
     var $amount = $("#newEntry").val();
     var $amountToInteger = parseInt($amount);
     total += $amountToInteger;
-    $entries.append("<tr><td></td><td> $" + ($amountToInteger.toFixed(2)) + "</td></tr>");
+    $entries.append("<tr><td></td><td> $<span class='entry'>" + ($amountToInteger.toFixed(2)) + "</span><a href='#'>x</a></td></tr>");
     var $totalText = $("#total");
     $totalText.html("$" + (total.toFixed(2)));
-    // var numericTotal = parseFloat($total.split("$")[1]);
-    $("input").val("");
 
+    // delete entries
+    var $a = $("a");
+    $a.on("click", function(e) {
+      e.preventDefault();
+      $(this).closest('tr').remove();
+    });
+
+    // editing previous entries
+      // can edit but unable to make clickaway work
+    $tbody.on('click','td',function() {
+        var fieldText = $(this).text();
+        var input = $('<input value="'+fieldText+'"/>');
+        $(this).text('').append(input);
+        input.focus();
+    });
+
+    $tbody.on('blur','input',function() {
+        $(this).text($(this).val());
+    });
+
+    $("input").val("");
   }
 
 });
