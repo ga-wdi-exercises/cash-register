9b1de5d88c1a3a37ac81edbaf006113632edaae0
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-07-06 15:16:16 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  // listen for form submission
+  $("#entry").on("submit", function(evt){
+    // prevent default action
+    evt.preventDefault();
+    // get user input && convert to float with 2 decimal placess
+    var amount = $("#newEntry").val();
+    var floatAmount = parseFloat(amount).toFixed(2);
+    console.log(floatAmount);
+    // place user input in table
+    // One tr, containing 2 td's, final td has amount
+    var newRow = $("<tr></tr>");
+    newRow.append($("<td></td>"));
+    newRow.append($("<td>" + floatAmount + "</td>"));
+    $("#entries").append(newRow);
+    // update total
+      //  get current total
+    var total = $("#total").html();
+    var numericTotal = parseFloat(total.split("$")[1]);
+    var actualTotal = floatAmount + numericTotal;
+
+//var balance = parseInt($(actualTotal).val() + $(amount).val());
+//$("#total").val(balance);
+
+//if (isNaN(balance)) {
+//  balance = 0;
+//}
+
+
+
+
+    console.log(balance);
+
+    $("input#newEntry").val("")
+  })
+    // make sure dollar signs are there
+
+})
