496c904f6bacf0187e7104cb5f9bc78e87ca2b93
Taylor Edwards <tedwards1884@gmail.com>
2016-07-06 18:41:38 -0400

    clear on empty return

--- js/main.js
+++ js/main.js
@@ -1,32 +1,44 @@
-var numbers = [];
-
 $('document').ready(function(){
+
+var numbers = [];
 
   $('#entry').submit(function(evt){
     evt.preventDefault();
+
     var enteredNum = $('#newEntry').val();
-    var dollarNum = getDollars(enteredNum);
 
-    var newRow = $("<tr></tr>");
-    newRow.append($('<td></td>'));
-    newRow.append($('<td>' + dollarNum + '</td>'));
-    $('#entries').append(newRow);
+// clears register on empty carriage return
 
-    numbers.push(dollarNum);
-    $('#newEntry').val('');
+    if (enteredNum == ""){
 
-    var sum = 0
-    for (i=0; i<numbers.length; i++) {
-      sum += numbers[i];
+      numbers = [];
+      $('#total').html('$0.00');
+      $('#entries').html('');
+
+    } else {
+
+      var dollarNum = getDollars(enteredNum);
+
+      var newRow = $("<tr></tr>");
+      newRow.append($('<td></td>'));
+      newRow.append($('<td>' + dollarNum + '</td>'));
+      $('#entries').append(newRow);
+
+      numbers.push(dollarNum);
+      $('#newEntry').val('');
+
+      var sum = 0;
+      for (i=0; i<numbers.length; i++) {
+        sum += numbers[i];
+      }
+
+      $('#total').html("$" + sum);
+
+      function getDollars(num) {
+        var convertedNum = parseFloat(num);
+        var finalNum = Math.round(convertedNum * 100) / 100;
+        return finalNum;
+      }
     }
-
-    $('#total').html("$" + sum);
-
-    function getDollars(num) {
-      var convertedNum = parseFloat(num);
-      var finalNum = Math.round(convertedNum * 100) / 100;
-      return finalNum;
-    }
-    
   })
 })
