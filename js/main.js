10358431b9fbb84bc3d7f24907c3c814fc7add8e
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-07-06 19:24:14 -0400

    walked through for extra understanding, adding comments

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ append a new <paragraph> with content = variable + existing value*/
 
 //listen for form submission
 $("#entry").on("submit", function(e){
-  //prevent default action (because its a form, wants to clear)
+  //prevent default action (because its a form, wants to reload and you'd lose data)
   e.preventDefault();
 //get user input
 var amount = $("#newEntry").val();
@@ -12,27 +12,31 @@ var amount = $("#newEntry").val();
 var floatAmount = parseFloat(amount).toFixed(2);
 
 //place user input in table
-  // one tr, containing 2 td's, final td has amount
-var newRow = $("<tr></tr>")
-newRow.append( $("<td></td>") )
-//make sure dollar signs are there
+  // add table row, and append data cells to that row
+var newRow = $("<tr></tr>");
+//empty data cell
+newRow.append( $("<td></td>") );
+//data cell that includes the $ float amount
 newRow.append( $("<td>" + "$" + floatAmount + "</td>") )
+//give that table body the new row
 $("#entries").append(newRow)
 //converting back to float
 floatAmount = parseFloat(floatAmount);
 //checking to make sure it's a number, not a string
 console.log(typeof(floatAmount));
-//get and update total
+//getting the inner html of the #total
 var total = $("#total").html()
-var numericalTotal = parseFloat(total.split("$")[1])
+//taking that total, split it out to an array, access only the part we want)
+var numericalTotal = parseFloat(total.split("$")[1]);
 console.log(typeof(numericalTotal));
+// take the new entry that we formatted, add it to the numericaltotal which is the running total
 var actualTotal = floatAmount + numericalTotal
 console.log(typeof(actualTotal));
+//take what we just added, convert to string
 actualTotal = actualTotal.toFixed(2);
 console.log(typeof(actualTotal), actualTotal);
+//change the inner html for #total
 $("#total").html("$" + actualTotal);
-
-
 //clear field
 $("#newEntry").val("")
 })
