d04696dde479fe943e373a9e940b4dccc7ec0aa6
johnestes4 <johnestes4@gmail.com>
2016-07-06 16:52:14 -0400

    Added if statement to prevent NaN inputs from being added

--- js/main.js
+++ js/main.js
@@ -4,15 +4,21 @@ $('document').ready(function(){
 
     var amountIn = parseFloat($('#newEntry').val());
     var amount = parseFloat(amountIn);
-    var newRow = $('<tr></tr>');
-    newRow.append($('<td></td>'));
-    newRow.append($('<td>' + amount.toFixed(2) + '</td>'));
-    $('#entries').append(newRow);
-    var total = $('#total').html();
-    var numericTotal = parseFloat(total.split('$')[1]);
-    var actualTotal = (numericTotal + amount).toFixed(2);
-    $('#total').text('$' + actualTotal);
-    $('input#newEntry').val("");
+    if (isNaN(amount) == false){
+      var amount = parseFloat(amountIn);
+      var newRow = $('<tr></tr>');
+      newRow.append($('<td></td>'));
+      newRow.append($('<td>' + amount.toFixed(2) + '</td>'));
+      $('#entries').append(newRow);
+      var total = $('#total').html();
+      var numericTotal = parseFloat(total.split('$')[1]);
+      var actualTotal = (numericTotal + amount).toFixed(2);
+      $('#total').text('$' + actualTotal);
+      $('input#newEntry').val("");
+    }
+    else if (isNaN(amount)) {
+      $('input#newEntry').val("");
+    }
   })
 
 });
