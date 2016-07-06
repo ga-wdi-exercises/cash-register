a530bce7f8281c36c26671a242d271cfc2a0a057
Jacqui Ahn <jsahn23@gmail.com>
2016-07-06 14:32:15 -0400

    starts the file

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  console.log("I'm ready");
+  // listen for form submission
+  $("#entry").on("submit", function(evt){
+    // prevent default action
+    evt.preventDefault()
+    // get user input && convert to float with 2 decimal placess
+    var amount = $("#newEntry").val()
+    var floatAmount = parseFloat(amount).toFixed(2)
+    console.log(floatAmount);
+    // place user input in table
+    // One tr, containing 2 td's, final td has amount
+    var newRow = $("<tr></tr>")
+    newRow.append($("<td></td>"))
+    newRow.append($("<td>" + floatAmount + "</td>"))
+    $("#entries").append(newRow)
+    // update total
+      //  get current total
+    var total = $("#total").html()
+    var numericTotal = parseFloat(total.split("$")[1])
+    var actualTotal = floatAmount + numericTotal
+    console.log();
+    $("input#newEntry").val("")
+  })
+
+    // make sure dollar signs are there
+
+})
