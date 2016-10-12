f967c4708ff080431ab90d916354d2d40b8d7a59
Rob Romaniw <robertromaniw@gmail.com>
2016-10-12 14:13:40 -0400

    Commit 2: Still doing basic adding - Added 2 decimal points

--- js/main.js
+++ js/main.js
@@ -3,16 +3,20 @@ $( document ).ready(function() {
 
 var registerBalance = 0;
 
-//listen for form submission
-$("#entry").on("submit", function(evt){
-    //prevent the default application
-    evt.preventDefault();
-    //capture user input
-    var amount = Number($("#newEntry").val()); //Captures user input, turns it into a number
-    registerBalance += amount; // Updates registerBalance variable
-    $("#total").html("$ " + registerBalance);
 
+$("#entry").on("submit", function(evt){ //listen for form submission
+    evt.preventDefault(); //prevent the default application
+    var entry = Number($("#newEntry").val()); //Captures user entry, turns it into a number
+    registerBalance += entry; //Updates registerBalance variable
+    $("#total").html("$ " + registerBalance.toFixed(2)); //Displays registerBalance
+
+
+
+    // registerBalance += amount; // Updates registerBalance variable
+    // $("#total").html("$ " + registerBalance);
 });
+
+
 
 
 
