8d25a767ecb9a827024f1fb1c68a4b411d5d6d12
Robert Zehnder <robzcodes@gmail.com>
2016-10-12 19:02:13 -0400

    Completes cash register with both bonuses

--- js/main.js
+++ js/main.js
@@ -7,78 +7,89 @@ $( document ).ready(function() {
 var total = 0;
 //var balanceSheet = [];
 
-  //Captures User Input
+//------------------------Captures User Input------------------------
+
   $("#entry" ).on('submit', function(evt) {
     evt.preventDefault();
 
     var amount = $('#newEntry').val();
     total += Number(amount);
 
-    //Appends line item
-    $("#entries").append("<tr><td></td><td>" + amount + ".00 </td></tr>");
+    $("#entries").append("<tr><td></td><td contenteditable='true'>" + amount + ".00 </td></tr>"); //Appends line item
 
-    //Removes number from Entry field
-    $('#newEntry').val("");
+    $('#newEntry').val("");//Removes number from Entry field
 
-    //Update Total field
-    $('#total').html("$" + total + ".00");
+    updateTotal();//Update Total field
 
   });
-  // - User clicks button
+
+//------------------------Delete Button------------------------
+
   $("#delete" ).on('click', function(evt) {
     evt.preventDefault();
 
-    // - Delete Button appears in column next to transaction
-
-    $( "#entries tr" ).each(function( ) {
-      //Put the button next to the transaction
-      $(this).children().first().html("<input type='submit' id='stuff' value='Delete Entry'>");
-
+      $( "#entries tr" ).each(function( ) { // - Delete Button appears in column next to transaction
+        $(this).children().first().html("<input type='submit' id='deleteRow' value='Delete Entry'>");
     });
 
-    //Create Event for deleting transaction
+//------------------------Delete Transaction------------------------
     $('#entries tr td').children().on('click',function(evt){
       evt.preventDefault();
-      //Delete Total
-      var numHolder = $(this).parent().next().text(); //'#entries tr td'
 
-      total -= Number(numHolder);
-      //Update Total field
-      $('#total').html("$" + total + ".00");
-      //Delete Row
-      $(this).closest('tr').remove();
+      var numHolder = $(this).parent().next().text(); //Get the value from the row that was clicked on
+      total -= Number(numHolder); //Deduct from total
+
+      updateTotal(); //Update Total field
+
+      $(this).closest('tr').remove(); //Delete Row
     })
   });
 
-//Create Event for Editing Transaction
+//------------------------Create Edit Buttons------------------------
 
-$("#edit" ).on('click', function(evt) {
-  evt.preventDefault();
-
-  // - Edit Button appears in column next to transaction
-
-  $( "#entries tr" ).each(function( ) {
-    //Put the button next to the transaction
-    $(this).children().first().html("<input type='submit' id='stuff' value='Edit Entry'>");
-
-  });
-
-  //Create Event for editing transaction
-  $('#entries tr td').children().on('click',function(evt){
+  $("#edit" ).on('click', function(evt) {
     evt.preventDefault();
 
-    //Edit Total
-    var numHolder = $(this).parent().next().text(); //'#entries tr td'
+    $( "#entries tr" ).each(function( ) { //Adds edit button to transaction table
+      $(this).children().first().html("<input type='submit' id='edit' value='Edit Entry'>");
 
-    total -= Number(numHolder); //Need to fix
+    });
 
-    //Update Total field
-    $('#total').html("$" + total + ".00");
+//------------------------Listener for Edit Buttons------------------------
 
-    //Edit the total in the same column
+    $('#entries tr td').children().on('click',function(evt){
+      evt.preventDefault();
 
-  })
-});
+      //Edit Total
+      var numHolder = $(this).parent().next().text(); //Gets the current value prior to editing
+      $(this).parent().next().css('background-color', '#FAFAD2'); //highlights field you want to edit
+      $(this).parent().html("<input type='submit' id='save' value='Save Entry'>") //Changes the edit button to a save button
 
+//------------------------Listener for Saving the Edited transaction------------------------
+
+        $('#save').on('click',function functionName(evt) { //Function to save the changes you made
+          evt.preventDefault();
+          var num2 = $(this).parent().next().text(); //setting the num var to your current total
+          total += Number(num2); //Adds in the new change
+          total -= Number(numHolder); //Removes old value
+          updateTotal(); //Update Total field
+          hideButtons();
+        });
+    })
+  });
+
+//------------------------Hides Buttons------------------------
+
+function hideButtons(){
+    $( "#entries tr" ).each(function( ) { // - Removes the edit buttons
+      $(this).children().first().html("");
+          });
+}
+
+//------------------------Updates the Total displayed to user------------------------
+
+  function updateTotal() {
+    $('#total').html("$" + total + ".00"); //Update Total field
+  }
 
 });
