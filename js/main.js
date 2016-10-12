291359f40e4dc1cd9f39e2ebcb03e43b401a9d56
Jonathan Grunewald <jonathan.grunewald1@gmail.com>
2016-10-12 15:59:31 -0400

    attmeps bonus #1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
 
 //listen for form submission
+
+//how could i restructure code without muddying up global variable space?
+
 var total = 0;
 $("#entry").on("submit", function(evt){
 	
 	//check input is a number
 if(isNaN($("input").val()) || $("input").val() === ""){
 	alert("Please enter numbers only");
+	$("input").val("");
 	evt.preventDefault();
 
 } else{
@@ -22,7 +26,7 @@ if(isNaN($("input").val()) || $("input").val() === ""){
 	// console.log(amount)
 
 	//log entry
-	$("#entries").append("<tr><td></td><td>" + amount.toFixed(2) + "</td></tr>");
+	$("#entries").append("<tr class='newValue'><td></td><td>" + amount.toFixed(2) + "</td></tr>");
 
 	// //update total
 	total += amount;
@@ -30,6 +34,23 @@ if(isNaN($("input").val()) || $("input").val() === ""){
 
 	//clear input
 	$("input").val("");
+
+
+	//add click event to table rows
+	$(".newValue").on("click", deleteValue);
 }
 
-})})
+})
+
+//this occasionally deletes the value twice.. not sure why..
+function deleteValue(){
+	console.log($(this).text());
+	var deletedAmount = parseFloat($(this).text());
+	console.log(deletedAmount);
+	total -= deletedAmount;
+	console.log(total);
+	$("#total").html("$"+total.toFixed(2));
+	$(this).remove();
+
+}
+})
