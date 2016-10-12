c1f3449cdf66e22e00e1094eef792b1a1e5d192e
Robert Zehnder <robzcodes@gmail.com>
2016-10-12 10:02:42 -0400

    Adds edit button, need to add edit field and update total

--- js/main.js
+++ js/main.js
@@ -34,10 +34,6 @@ var total = 0;
       //Put the button next to the transaction
       $(this).children().first().html("<input type='submit' id='stuff' value='Delete Entry'>");
 
-      //var number = $( this ).text(); //figure out if you can pull the number with .text - yes, need to convert to #
-      //console.log(Number(number));
-
-
     });
 
     //Create Event for deleting transaction
@@ -45,22 +41,44 @@ var total = 0;
       evt.preventDefault();
       //Delete Total
       var numHolder = $(this).parent().next().text(); //'#entries tr td'
-      
+
       total -= Number(numHolder);
       //Update Total field
       $('#total').html("$" + total + ".00");
       //Delete Row
       $(this).closest('tr').remove();
     })
+  });
 
+//Create Event for Editing Transaction
 
-    // - Need to query the row # and capture it
-    // - When delete button is pressed, the row is removed based on the index captured
-    // - total is edited to reflect the new total
+$("#edit" ).on('click', function(evt) {
+  evt.preventDefault();
 
+  // - Edit Button appears in column next to transaction
+
+  $( "#entries tr" ).each(function( ) {
+    //Put the button next to the transaction
+    $(this).children().first().html("<input type='submit' id='stuff' value='Edit Entry'>");
 
   });
 
+  //Create Event for editing transaction
+  $('#entries tr td').children().on('click',function(evt){
+    evt.preventDefault();
+
+    //Edit Total
+    var numHolder = $(this).parent().next().text(); //'#entries tr td'
+
+    total -= Number(numHolder); //Need to fix
+
+    //Update Total field
+    $('#total').html("$" + total + ".00");
+
+    //Edit the total in the same column
+
+  })
+});
 
 
 });
