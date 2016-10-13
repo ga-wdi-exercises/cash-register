31d2b6c6a5e879c6296ba9c46313bb85dc326bca
Bradley Cravens <bradleycravens@gmail.com>
2016-10-13 08:42:06 -0400

    inital commit

--- js/main.js
+++ js/main.js
@@ -5,13 +5,35 @@ var total = 0;
     $("#entry").on("submit", function(pd){
     pd.preventDefault();
     var userInput = Number($("#newEntry").val());
-    total += userInput;
-    var newEntry = $("<tr><td><td>"+userInput.toFixed(2)+"</td></td></tr>");
-    $("#entries").append(newEntry);
-    $("#total").html("$"+total.toFixed(2));
-    $("#entry").trigger("reset");
+      if (userInput > 0) {
+        total += userInput;
+        var newEntry = $("<tr><td><td>"+userInput.toFixed(2)+"</td></td></tr>");
+        $("#entries").append(newEntry);
+        $("#total").html("$"+total.toFixed(2));
+        $("#entry").trigger("reset");
+      } else {
+        $("#entry").trigger("reset");
+      }
     });
   }
 
+  function dltButtons() {
+    $("#dlt").on("click", function(pd) {
+      pd.preventDefault();
+      $("#entries tr").each(function() {
+        $(this).children().first().html("<input type='submit' id='dlts' value='Delete Entry'>");
+      });
+    });
+  }
+
+  function dltEntry() {
+    $('#dlts').on('click',function(pd){
+        pd.preventDefault();
+        console.log("it works");
+      });
+  }
+
 register();
+dltButtons();
+dltEntry();
 });
