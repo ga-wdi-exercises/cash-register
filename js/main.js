2742504bc9c90272ec05f1ccc8f1f4ac7d72ebe3
Rob Romaniw <robertromaniw@gmail.com>
2016-10-12 22:06:34 -0400

    Commit 3: Appended individual transactions

--- js/main.js
+++ js/main.js
@@ -1,33 +1,16 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
+$(document).ready(function() {
 
-var registerBalance = 0;
-
-
-$("#entry").on("submit", function(evt){ //listen for form submission
-    evt.preventDefault(); //prevent the default application
-    var entry = Number($("#newEntry").val()); //Captures user entry, turns it into a number
-    registerBalance += entry; //Updates registerBalance variable
-    $("#total").html("$ " + registerBalance.toFixed(2)); //Displays registerBalance
+    var registerBalance = 0; //set global register Balance variable
 
 
-
-    // registerBalance += amount; // Updates registerBalance variable
-    // $("#total").html("$ " + registerBalance);
-});
-
-
-
-
-
+    $("#entry").on("submit", function(evt) { //listen for form submission
+        evt.preventDefault(); //prevent the default application
+        var entry = Number($("#newEntry").val()); //Captures user entry, turns it into a number
+        registerBalance += entry; //Updates registerBalance variable
+        $("#entries").append("<tr><th></th><th>" + "$" + entry.toFixed(2) + "</th></tr>"); // Appends line item
+        $('#newEntry').val(""); // Clears value from input entry
+        $('#total').html("$" + registerBalance.toFixed(2)); // updates total to registerBalance
 
 
-
-
-
-
-
-
-
-
+    });
 });
