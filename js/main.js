7a050a496ff04a348935cc1e4044c90dec65a8b3
Juliet Berman <berman.juliet@gmail.com>
2016-10-13 15:58:03 -0400

    completed bonus #1

--- js/main.js
+++ js/main.js
@@ -12,20 +12,24 @@ $("form").on("submit", function(event){
       cashRegister.splice(0, 1, total);
       $("input#newEntry").val("");
 
-      $("tbody#entries").append("<tr><td><button>remove item</button></td><td>" + "$" + amount.toFixed(2) + "</td></tr>");
+      $("tbody#entries").append("<tr><td><button>remove item</button></td><td contenteditable='true'>" + "$" + amount.toFixed(2) + "</td></tr>");
 
       $("#total").text("$" + total.toFixed(2));
 
-      $("button").on("click", function(){
-          var $item = $(this).closest("tr").remove();
-          //                     .find("td")
-          //                     .text();
-          // var newTotal = cashRegister[0] - ($item.match(/\d+/));
-          // cashRegister.splice(0, 1, newTotal);
-          // // $("#total").text("$" + newTotal.toFixed(2));
-          // console.log(cashRegister);
-          });
-  }
+      $("button").last().on("click", function(){
+          var item = $(this).parent()
+                            .siblings()
+                            .last()
+                            .text()
+                            .replace("$","");
+
+          var number = parseFloat(item);
+          var newTotal = cashRegister[0] - number;
+          cashRegister.splice(0, 1, newTotal);
+          $("#total").text("$" + newTotal.toFixed(2));
+        $(this).parent().parent().remove();
+        });
+}
   else {
     alert("Please enter a number");
     $("input#newEntry").val("");
