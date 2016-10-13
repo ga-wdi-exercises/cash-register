a13fa0f1d4981aa430272dd9f8b4580b23ed73f7
Sam Mans <smans123@gmail.com>
2016-10-13 08:59:57 -0400

    fixed the adding system

--- js/main.js
+++ js/main.js
@@ -1,23 +1,32 @@
 //listen for form submission
+var total = 0;
+
 $("#entry").on("submit", function(evt){
   //prevent the default action
   evt.preventDefault()
 
   //capture user input
-  var amount = $("#newEntry").val();
+  // var amount = $("#newEntry").val();
   var entry = $("#total");
   var pastEntries = $("#entries");
+
+
   console.log(amount);
 
   //converted to a number
-  amount = parseFloat(amount).toFixed(2);
+  var amount = parseFloat($("#newEntry").val());
   console.log(amount)
+  pastEntries.append("<tr><td></td><td class=past>" + amount + "</td></tr>");
 
-  entry.html("$ " + amount);
+  total = total + amount
+
+  entry.html("$ " + total.toFixed(2));
+
 console.log("this amount is " +amount)
 
 
-  pastEntries.append("<tr><td></td><td class=past>" + amount + "</td></tr>");
+
+
 
 
 
